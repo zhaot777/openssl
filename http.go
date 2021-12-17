6365946d9b4a09e1c5164a971b571c99a06ea8b4
Tong Zhao 🍵 <zhatong@amazon.com>
2021-12-17 13:30:53 -0800

    Disable the http and net functions

--- http.go
+++ http.go
@@ -15,39 +15,38 @@
 package openssl
 
 import (
-	"net/http"
-	"github.com/zhaot777/openssl"
+	// "net/http"
 )
 
-// ListenAndServeTLS will take an http.Handler and serve it using OpenSSL over
-// the given tcp address, configured to use the provided cert and key files.
-func ListenAndServeTLS(addr string, cert_file string, key_file string,
-	handler http.Handler) error {
-	return ServerListenAndServeTLS(
-		&http.Server{Addr: addr, Handler: handler}, cert_file, key_file)
-}
+// // ListenAndServeTLS will take an http.Handler and serve it using OpenSSL over
+// // the given tcp address, configured to use the provided cert and key files.
+// func ListenAndServeTLS(addr string, cert_file string, key_file string,
+// 	handler http.Handler) error {
+// 	return ServerListenAndServeTLS(
+// 		&http.Server{Addr: addr, Handler: handler}, cert_file, key_file)
+// }
 
-// ServerListenAndServeTLS will take an http.Server and serve it using OpenSSL
-// configured to use the provided cert and key files.
-func ServerListenAndServeTLS(srv *http.Server,
-	cert_file, key_file string) error {
-	addr := srv.Addr
-	if addr == "" {
-		addr = ":https"
-	}
+// // ServerListenAndServeTLS will take an http.Server and serve it using OpenSSL
+// // configured to use the provided cert and key files.
+// func ServerListenAndServeTLS(srv *http.Server,
+// 	cert_file, key_file string) error {
+// 	addr := srv.Addr
+// 	if addr == "" {
+// 		addr = ":https"
+// 	}
 
-	ctx, err := NewCtxFromFiles(cert_file, key_file)
-	if err != nil {
-		return err
-	}
+// 	ctx, err := NewCtxFromFiles(cert_file, key_file)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	l, err := Listen("tcp", addr, ctx)
-	if err != nil {
-		return err
-	}
+// 	l, err := Listen("tcp", addr, ctx)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return srv.Serve(l)
-}
+// 	return srv.Serve(l)
+// }
 
 // TODO: http client integration
 // holy crap, getting this integrated nicely with the Go stdlib HTTP client
